8bd48623588fac6446ce5f5df8198f3f5c60a3ba
blattersturm <peachypies@protonmail.ch>
2023-02-15 15:48:10 +0300

    refactoring code

--- include/Toolkit/Toolkit.hpp
+++ include/Toolkit/Toolkit.hpp
@@ -3,20 +3,20 @@
 #include <iostream>
 
 namespace ii {
-
-	uint64_t g_toolkitResult;
-
 	class Toolkit {
 	public:
 		template<typename T>
 		__forceinline static T GetAs() noexcept {
-			return (T)(g_toolkitResult);
+			return (T)(g_result);
 		}
 
 	protected:
 		template<typename T>
 		__forceinline static void SetAs(T result) noexcept {
-			g_toolkitResult = (uint64_t)result;
+			g_result = (uint64_t)result;
 		}
+
+	private:
+		inline static uint64_t g_result = 0;
 	};
 }
