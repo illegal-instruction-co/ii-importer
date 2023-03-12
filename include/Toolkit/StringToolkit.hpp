54afa0d4d8a4d3bddecff4330786390f66b92523
illegal-instruction-co <machinetherapist2@gmail.com>
2023-03-12 21:14:01 +0300

    trust NRVO
    This discrepancy was discovered by unaimed1337's comment.

--- include/Toolkit/StringToolkit.hpp
+++ include/Toolkit/StringToolkit.hpp
@@ -3,7 +3,6 @@
 #include "Toolkit.hpp"
 
 #include <Windows.h>
-#include <iostream>
 
 namespace ii {
 	class StringToolkit final : public Toolkit {
@@ -11,23 +10,29 @@ namespace ii {
 		static StringToolkit IsReadable(char* str) {
 			if (str == nullptr)
 				SetAs<bool>(false);
+
 			MEMORY_BASIC_INFORMATION mbi;
+
 			VirtualQuery(str, &mbi, sizeof(mbi));
+
 			SetAs<bool>(mbi.Protect & PAGE_READONLY || mbi.Protect & PAGE_READWRITE);
-			return std::move(StringToolkit());
+
+			return StringToolkit();
 		}
 
 		static StringToolkit IsAlphaNumeric(char* str) {
 			if (str == nullptr)
 				SetAs<bool>(false);
+
 			int len = sizeof(str);
+
 			for (int i = 0; i < len; i++)
 				if (!(isalpha(str[i]) || isdigit(str[i]) && (str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z')) || str[i] < 0 || str[i] > 127)
 					SetAs<bool>(false);
 
 			SetAs<bool>(true);
 
-			return std::move(StringToolkit());
+			return StringToolkit();
 		}
 	};
 }
