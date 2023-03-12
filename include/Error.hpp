4c8ebc03f428e33fbb8224d15173df08dde3f70b
illegal-instruction-co <machinetherapist2@gmail.com>
2023-03-12 21:15:51 +0300

    implement error messages in another header file

---
+++ include/Error.hpp
@@ -0,0 +1,10 @@
+#include <string>
+
+namespace ii::err {
+	const std::string runtime1 = "We could not export from module, because it was not loaded. Module: %s";
+	const std::string runtime2 = "Dos header is wrong of module ";
+	const std::string runtime3 = "Could not found nt header for ";
+	const std::string runtime4 = "Export directory could not read for ";
+	const std::string runtime5 = "Export directory was empty for ";
+	const std::string runtime6 = " not found in module ";
+}
