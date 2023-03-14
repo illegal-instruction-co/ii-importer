02796d4d8202ca61edf76dd3484d56a131c67dfb
illegal-instruction-co <machinetherapist2@gmail.com>
2023-03-14 18:01:16 +0300

    simplify the example

--- Example.cpp
+++ Example.cpp
@@ -9,9 +9,12 @@ using namespace std;
 
 DWORD WINAPI TheThread(void*)
 {
+	auto ntdll = ii::Importer("ntdll.dll");
+
 	for (;;) {
-		cout << "I am the thread" << endl;
-		this_thread::sleep_for(chrono::seconds(1));
+		cout << "Current processor number: " << ntdll.Invoke<int>("NtGetCurrentProcessorNumber")() << endl;
+
+		this_thread::sleep_for(chrono::milliseconds(500));
 	}
 
 	return 0;
@@ -19,17 +22,10 @@ DWORD WINAPI TheThread(void*)
 
 int main()
 {
-	cout << "Kernelbase base address is: ";
-
 	auto kernel32 = ii::Importer("Kernel32.dll");
-	cout << kernel32.Invoke<HMODULE>("GetModuleHandleA")("Kernelbase.dll") << endl;
-
 	const void* tHandle = kernel32.Invoke<void*>("CreateThread")(NULL, 0, TheThread, NULL, 0, NULL);
 
 	cout << "CreateThread returned: " << tHandle << endl;
-
-	auto ntdll = ii::Importer("ntdll.dll");
-	cout << "Current processor number: " << ntdll.Invoke<int>("NtGetCurrentProcessorNumber")() << endl;
 
 	return getchar();
 }
